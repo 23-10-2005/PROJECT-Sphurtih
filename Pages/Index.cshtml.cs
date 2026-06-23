edce156774e238db6290319f727943b47896f6a9
Exam <Exam@cyber.cst>
2026-06-23 13:21:31 +0530

    intial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace team_sphurtih.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
